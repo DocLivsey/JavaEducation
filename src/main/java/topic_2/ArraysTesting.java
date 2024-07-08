7ce4ee92be065375edb923ec3404cf4f1363389f
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-08 14:36:25 +0300

    create file for testing and learning java arrays and classes like Array and Arrays

---
+++ src/main/java/topic_2/ArraysTesting.java
@@ -0,0 +1,10 @@
+package topic_2;
+
+public class ArraysTesting {
+    public static void main(String[] args) {
+        int cStyleArray[] = new int[3]; // C|C++ array declaration style
+        int[] javaStyleArray = new int[3]; // Java array declaration style
+
+
+    }
+}
