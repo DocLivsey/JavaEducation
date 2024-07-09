fcf0e5d52793ea85f8e0c5b8fb8c8d8b5c1e23f2
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-09 12:17:17 +0300

    testing java Year

--- src/main/java/topic_2/java_object_oriented_programming/ObjectOrientedProgrammingTraining.java
+++ src/main/java/topic_2/java_object_oriented_programming/ObjectOrientedProgrammingTraining.java
@@ -1,4 +1,12 @@
 package topic_2.java_object_oriented_programming;
 
+import java.time.Year;
+
 public class ObjectOrientedProgrammingTraining {
+    public static void main(String[] args) {
+        int year = Year.now().getValue();
+        System.out.println(year);
+        System.out.println(year > 2024);
+    }
+
 }
