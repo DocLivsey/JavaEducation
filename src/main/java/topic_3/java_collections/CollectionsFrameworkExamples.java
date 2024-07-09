4585cef0de30ae85c666af1e7cd84f072e914f4e
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-09 15:07:00 +0300

    add example for arraylist

--- src/main/java/topic_3/java_collections/CollectionsFrameworkExamples.java
+++ src/main/java/topic_3/java_collections/CollectionsFrameworkExamples.java
@@ -1,4 +1,30 @@
 package topic_3.java_collections;
 
+import java.util.ArrayList;
+
 public class CollectionsFrameworkExamples {
+
+    public static void arrayListExamples() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("John");
+        list.add("Jane");
+        list.add("Bob");
+        list.add("Jack");
+
+        System.out.println(list);
+
+        list.removeFirst();
+        list.removeLast();
+
+        System.out.println(list);
+
+        list.add("Vladimir");
+        list.add("Alex");
+
+        System.out.println(list);
+
+        list.remove("Jane");
+
+        System.out.println(list);
+    }
 }
