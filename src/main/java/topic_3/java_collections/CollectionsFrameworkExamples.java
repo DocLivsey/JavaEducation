b9d90ae783614e0f1ae79102c3cce2d80d24f781
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-09 15:29:00 +0300

    add method for randomly generating generics arraylists via cycle

--- src/main/java/topic_3/java_collections/CollectionsFrameworkExamples.java
+++ src/main/java/topic_3/java_collections/CollectionsFrameworkExamples.java
@@ -1,8 +1,15 @@
 package topic_3.java_collections;
 
 import java.util.ArrayList;
+import java.util.Random;
+
+import org.jeasy.random.EasyRandom;
 
 public class CollectionsFrameworkExamples {
+    public static void main(String[] args) {
+        ArrayList<String> list = generateArrayList(String.class);
+        System.out.println(list);
+    }
 
     public static void arrayListExamples() {
         ArrayList<String> list = new ArrayList<>();
@@ -26,5 +33,20 @@ public class CollectionsFrameworkExamples {
         list.remove("Jane");
 
         System.out.println(list);
+    }
+
+    public static<T> ArrayList<T> generateArrayList(Class<T> tClass) {
+        Random random = new Random();
+        int size = random.nextInt(10);
+
+        ArrayList<T> list = new ArrayList<>(size);
+
+        EasyRandom generator = new EasyRandom();
+
+        for (int i = 0; i < size; i++) {
+            list.add(generator.nextObject(tClass));
+        }
+
+        return list;
     }
 }
