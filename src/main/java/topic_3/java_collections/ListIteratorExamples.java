46621876c5ddabf1411a6612d450d2e79bac88f4
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-09 15:38:14 +0300

    add examples for listIterator

--- src/main/java/topic_3/java_collections/ListIteratorExamples.java
+++ src/main/java/topic_3/java_collections/ListIteratorExamples.java
@@ -1,4 +1,26 @@
 package topic_3.java_collections;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class ListIteratorExamples {
+    public static void main(String[] args) {
+        iteratorExample();
+    }
+
+    public static void iteratorExample() {
+        ArrayList<String> list = CollectionsFrameworkExamples.generateArrayList(String.class);
+        System.out.println(list);
+
+        System.out.println("-->");
+        ListIterator<String> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            System.out.print(STR." \{listIterator.next()}");
+        }
+
+        System.out.println("\n<--");
+        while (listIterator.hasPrevious()) {
+            System.out.print(STR." \{listIterator.previous()}");
+        }
+    }
 }
