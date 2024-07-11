3f5dac08d9b47f64748e1ca4315c9eadffb4a4e0
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-11 23:32:04 +0300

    create new spring project by "Spring Initializer"
    some files and dir added by "Spring Initializer"
    
    added main app, tests and props

---
+++ DevEdu/src/test/java/dclvs/devedu/DevEduApplicationTests.java
@@ -0,0 +1,13 @@
+package dclvs.devedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
