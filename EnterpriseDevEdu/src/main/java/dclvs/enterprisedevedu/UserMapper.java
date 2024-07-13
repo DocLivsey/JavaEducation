9fd799d91a74db4d5458da38609d9131bb1cf62e
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-13 19:47:39 +0300

    add mapping annotation and method for convert date to long

--- EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserMapper.java
+++ EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserMapper.java
@@ -1,12 +1,25 @@
 package dclvs.enterprisedevedu;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Mapper
 public interface UserMapper {
 
+    @Mapping(target = "birthDate", ignore = true)
     User toEntity(UserDto dto);
 
     UserDto toDto(User entity);
+
+    default Long mapToCountDaysFromNow(LocalDate date) {
+        if (date == null) {
+            return null;
+        }
+
+        return date.until(LocalDate.now(), ChronoUnit.DAYS);
+    }
 
 }
