3b4e400fc2a88b931eb10bfe423dcf66ebbcfaca
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-13 19:16:16 +0300

    add field for mapping users and scheduled method

--- EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserService.java
+++ EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserService.java
@@ -1,4 +1,25 @@
 package dclvs.enterprisedevedu;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserMapper userMapper;
+
+    @Scheduled(cron = "* * * * * ?")
+    public void scheduledTask() {
+        User user = new User()
+                .setId(UUID.randomUUID())
+                .setFullName("Anton");
+        log.info("User = {}", user);
+        log.info("UserDto = {}", userMapper.toDto(user));
+    }
 }
