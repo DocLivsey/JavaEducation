94148d027c1ebe1c0b7c35fa610441939b973122
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-13 19:15:23 +0300

    add new annotation for chain construction of objects

--- EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/User.java
+++ EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/User.java
@@ -1,9 +1,12 @@
 package dclvs.enterprisedevedu;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
+
 import java.util.UUID;
 
 @Data
+@Accessors(chain = true)
 public class User {
     private UUID id;
     private String fullName;
--- EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserDto.java
+++ EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserDto.java
@@ -1,9 +1,12 @@
 package dclvs.enterprisedevedu;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
+
 import java.util.UUID;
 
 @Data
+@Accessors(chain = true)
 public class UserDto {
     private UUID id;
     private String fullName;
