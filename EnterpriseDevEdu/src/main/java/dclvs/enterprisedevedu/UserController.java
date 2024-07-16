8a5df56ab90ce97e80b43f84972ca62495c55315
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-16 14:06:14 +0300

    add method for logging users in swagger ui

--- EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserController.java
+++ EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserController.java
@@ -1,4 +1,19 @@
 package dclvs.enterprisedevedu;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/user/log")
+    public ResponseEntity<User> convertAndLogUser(@RequestBody UserDto user) {
+        userService.convertAndLog(user);
+        return ResponseEntity.ok().build();
+    }
+
 }
