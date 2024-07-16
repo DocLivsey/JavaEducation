409c8ce64364cd7e85ec8083c5edfb3e93eb4fe6
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-17 01:18:44 +0300

    add the annotation for documentation class and its fields in json format

--- EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserDto.java
+++ EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserDto.java
@@ -1,5 +1,6 @@
 package dclvs.enterprisedevedu;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -7,12 +8,16 @@ import java.util.UUID;
 
 @Data
 @Accessors(chain = true)
+@Schema(name = "Container for user data", description = "Данные пользователя")
 public class UserDto {
 
+    @Schema(name = "ID", description = "Идентификатор")
     private UUID id;
 
+    @Schema(description = "ФИО пользователя", deprecated = true)
     private String fullName;
 
+    @Schema(description = "Кол-во дней, прошедших со дня рождения", hidden = true)
     private Long birthDate;
 
 }
