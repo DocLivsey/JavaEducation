a1ac6a8ba206eb9e2240064f002fe686e547402e
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-17 16:55:01 +0300

    add lombok generation for field and other inherited methods
    add desc for schema in swagger

--- EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserWithBirthDataDto.java
+++ EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/UserWithBirthDataDto.java
@@ -1,4 +1,23 @@
 package dclvs.enterprisedevedu;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Accessors(chain = true)
+@Schema(description = "Данные пользователя с полной датой рождения")
 public class UserWithBirthDataDto {
+
+    @Schema(description = "Идентификатор")
+    private UUID id;
+
+    @Schema(description = "ФИО пользователя", example = "Ivanov Ivan Ivanovich")
+    private String fullName;
+
+    @Schema(description = "Дата рождения")
+    private LocalDate birthData;
+
 }
