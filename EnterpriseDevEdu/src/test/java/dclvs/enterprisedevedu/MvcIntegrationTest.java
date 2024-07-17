41ddfc037360d15b6334420e5a0df545d6f00bb8
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-17 16:57:37 +0300

    write classes for e2e testing

--- EnterpriseDevEdu/src/test/java/dclvs/enterprisedevedu/MvcIntegrationTest.java
+++ EnterpriseDevEdu/src/test/java/dclvs/enterprisedevedu/MvcIntegrationTest.java
@@ -1,4 +1,27 @@
 package dclvs.enterprisedevedu;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
 public class MvcIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
 }
--- EnterpriseDevEdu/src/test/java/dclvs/enterprisedevedu/UserControllerTest.java
+++ EnterpriseDevEdu/src/test/java/dclvs/enterprisedevedu/UserControllerTest.java
@@ -1,4 +1,54 @@
 package dclvs.enterprisedevedu;
 
-public class UserControllerTest {
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+public class UserControllerTest extends MvcIntegrationTest {
+
+    @SpyBean
+    private UserMapper userMapper;
+
+    @Test
+    @SneakyThrows
+    void testConvertUserToUserWithBirthDataReturnCorrectResponse() {
+        // Given
+        Mockito.when(userMapper.toEntity(Mockito.any())).thenReturn(buildTestUser());
+
+        // When
+        var actions = mockMvc.perform(MockMvcRequestBuilders.post("/user/convert/with-birth-data")
+                .content(objectMapper.writeValueAsBytes(buildTestUserDto()))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // Then
+        actions.andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id")
+                        .value("7c27a110-d271-4527-9085-911c982479df"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.birthData")
+                        .value(LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)));
+    }
+
+    private User buildTestUser() {
+        return new User()
+                .setId(UUID.fromString("7c27a110-d271-4527-9085-911c982479df"))
+                .setFullName("Vova")
+                .setBirthDate(LocalDate.now());
+    }
+
+    private UserDto buildTestUserDto() {
+        return new UserDto()
+                .setId(UUID.fromString("7c27a110-d271-4527-9085-911c982479df"))
+                .setFullName("Vova")
+                .setBirthDate(5L);
+    }
+
 }
