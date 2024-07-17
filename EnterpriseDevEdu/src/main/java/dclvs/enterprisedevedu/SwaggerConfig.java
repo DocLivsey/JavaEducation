50af723c0480effee375ee30fa6582e336d621b0
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-17 15:42:49 +0300

    added 'basic' auth to security requirements list

--- EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/SwaggerConfig.java
+++ EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/SwaggerConfig.java
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 @SecurityScheme(name = "basic", scheme = "basic", type = SecuritySchemeType.HTTP)
@@ -21,7 +23,8 @@ public class SwaggerConfig {
                         .version("0.0.1"))
                 .externalDocs(new ExternalDocumentation()
                         .description("Этот же сваггер")
-                        .url("http://localhost:8080/swagger-ui/index.html"));
+                        .url("http://localhost:8080/swagger-ui/index.html"))
+                .security(List.of(new SecurityRequirement().addList("basic")));
     }
 
 }
