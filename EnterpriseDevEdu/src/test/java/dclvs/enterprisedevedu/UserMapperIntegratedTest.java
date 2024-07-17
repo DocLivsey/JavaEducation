420baed256bc8adb6906566b24a77138081cdd37
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-17 16:19:52 +0300

    add some comments

--- EnterpriseDevEdu/src/test/java/dclvs/enterprisedevedu/UserMapperIntegratedTest.java
+++ EnterpriseDevEdu/src/test/java/dclvs/enterprisedevedu/UserMapperIntegratedTest.java
@@ -7,6 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
 import java.util.UUID;
 
+/*
+    - Внутренние интеграционные тесты тестируют логику приложения изнутри вместе с его развертыванием.
+    - e2e-тесты отличаются от внутренних интеграционных тем, что тестируют функционал не вызовами изнутри,
+        а запросами снаружи. e2e расшифровывается как “end to end” и связано с термином “endpoint”.
+        Проще говоря, e2e-тесты тестируют API приложения.
+ */
 @SpringBootTest
 public class UserMapperIntegratedTest {
 
