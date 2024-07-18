af1037ba8617cb3b653b09cae14d712b76b5f00e
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-18 17:05:27 +0300

    add new import

--- EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/WebSecurityConfig.java
+++ EnterpriseDevEdu/src/main/java/dclvs/enterprisedevedu/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,7 +25,7 @@ public class WebSecurityConfig {
                         .loginPage("/login") // Указывает страницу входа
                         .permitAll() // Разрешает доступ к форме входа для всех пользователей
                 )
-                .logout((logout) -> logout.permitAll()); // Разрешает всем пользователям выходить из системы
+                .logout(LogoutConfigurer::permitAll); // Разрешает всем пользователям выходить из системы
         return http.build();
     }
     @Bean
